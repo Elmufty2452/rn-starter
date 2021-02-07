ef25ec18a88f07bd167fcff517c148fbe59b3034
Abdullah Abdulfatah <boomjay008@gmail.com>
2021-02-07 02:05:48 -1200

    save

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,17 +4,17 @@ import { Text, StyleSheet, View, Button } from "react-native";
 const HomeScreen = (props) => {
   return (
     <View>
-      <Text style={styles.text}>HELLO DRAQULA</Text>,
+      <Text style={styles.text}>HELLO DRAQULA</Text>
       <Button
         style = {styles.Button}
         onPress = {() => props.navigation.navigate('Components')}
         title = "Go to Components Demo"
-      />,
+      />
       <Button
         style = {styles.Button}
         onPress = {() => props.navigation.navigate('List')}
         title = "Go to List Demo"
-      />,
+      />
       <Button
         style = {styles.Button}
         onPress = {() => console.log('Image')}
